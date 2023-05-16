8560d0059956cb0de06181daf9ff896462c259c9
wender.junior <wender.junior@etaure.com.br>
2023-05-16 12:45:21 -0300

    CreateJson

--- src/Components/pages/CreateForm.jsx
+++ src/Components/pages/CreateForm.jsx
@@ -1,9 +1,47 @@
+import { useRef, useState } from "react";
+
 import { Container } from "react-bootstrap";
 
 function CreateForm(){
+    // const number = useRef(null);
+
+    const [ inputNumber, setInputNumber] = useState();
+
+    function handleChange(e) {
+        setInputNumber(e.target.value);
+        // if (inputNumber > 4) {
+        //     setInputNumber(number.current.value);
+        // }
+    }
+
+    // function moreThenFour() {
+    //     setInputNumber(number.current.value);
+    //     console.log('inputNumber: ',inputNumber);
+    // }
+
     return(
         <Container>
             <h1 className="mt-4">Create Form</h1>
+            <label>Selecione Quantos Campos Terá Seu Formulário:</label>
+            <select className="d-flex mt-2" onChange={handleChange}>
+                <option value="">--</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            {/* {inputNumber === "mais" &&
+                <div>
+                    <label className="mt-3">Entre com a quantidade de campos:</label>
+                    <input type="number" ref={number} className="d-flex mt-2 "/>
+                    <button className="btn btn-primary mt-2" onClick={moreThenFour}>Enviar</button>
+                </div>
+            } */}
+            {
+                
+
+            }
         </Container>
     );
 }
