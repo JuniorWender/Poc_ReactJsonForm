fc30c37fb249b078f5f339994cc3ecd23ba1c019
wender.junior <wender.junior@etaure.com.br>
2023-05-16 14:30:07 -0300

    inputForm

--- src/Components/layout/InputForm.jsx
+++ src/Components/layout/InputForm.jsx
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
 
-function InputForm({ create, labelTitle, inputType, inputName, inputPlaceholder, inputMask}){
-    const createForm = ["Tipo Do Input", "Nome Do Campo", "Placeholder", "Mascara"];
+function InputForm({ create, numeroDeInput, labelTitle, inputType, inputName, inputPlaceholder, inputMask}){
+    const createForm = ["Tipo Do Input", "Nome Do Campo", "Placeholder", "Máscara"];
     const complemento = ["text/number...", "Nome Na Tag HTML", "O Placeholder Do Campo", "Tipo da Mascara (CNPJ, CPF, Telefone, Data, etc...)"]
 
     return(
@@ -12,10 +12,12 @@ function InputForm({ create, labelTitle, inputType, inputName, inputPlaceholder,
             <input type={inputType} name={inputName} placeholder={inputPlaceholder} mask={inputMask} />
             {create === true &&
                 createForm.map((element,key) => {
-                    <div key={key}>
-                        <label>{element[key]}</label>
-                        <input type="text" placeholder={complemento[key]} />
-                    </div>
+                    return(
+                        <div key={key}>
+                            <label className="mt-2">{`${element}:`}</label>
+                            <input className="d-flex" type="text" placeholder={complemento[key]} />
+                        </div>
+                    );
                 })
             }
         </Container>
